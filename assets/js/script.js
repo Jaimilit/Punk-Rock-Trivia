f7e937da7118a4cb46c930ea407f441f6fcbacef
Jaimilit <>
2023-05-01 13:20:04 +0000

    started adding functions for game -  display question and check answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,81 @@
-let triviaData = [
+let questions = [
     {
         question: "Jello Biaffara was frontman for which punk band?",
-        answer: "The Dead Kennedys",
         choices: ["The Clash", "The Dead Kennedys", "Rancid","NOFX"]
+        correctAnswer: 1
       },
       {
         question: "Which style of punk was developed by The Misfits?",
-        answer: "Horror Punk",
         choices: ["Emo Punk", "Hardcore Punk", "Oi Punk", "Horror Punk"]
+        correctAnswer: 3
       },
       {
         question: "In 1995, this band's breakthrough album, Dookie, won the Grammy for Best Alternative Music Album.",
-        answer: "Green Day",
         choices: ["Green Day", "Rancid", "The Offspring", "NOFX"]
+        correctAnswer: 0
       }
       question: "Sometimes, it's the saddest incidents that make for the most amazing of songs. Name the band who turned a suicide tragedy into "Bro Hymn".",
-      answer: "Pennywise",
       choices: ["Pennywise", "Rancid", "Bad Religion", "Operation Ivy"]
+      correctAnswer: 0
     },
     {
       question: "Milo went to college, and then Everything Sucked. Who is the band, who created these records, and could also be noted for the cartoon drawn about their lead singer, depicting him as an almost Dilbert-like character?",
-      answer: "The Descendents",
       choices: ["Leftover Crack", "The Descendents", "AFI", "The Unseen"]
+      correctAnswer: 1
     },
     {
       question: "Who is featured in Leftover Crack's 'System Fucked'?",
-      answer: "Jesse Michaels",
       choices: ["Lars Frederiksen", "Jesse Michaels", "Tim Armstrong", "Jack Terricloth"]
+      correctAnswer: 1
     }
     question: "This band was formed by Andy Outbreak and Eric Ozenne and their last album is titled 'The Butterfly Collection'",
-    answer: "The Nerve Agents",
     choices: ["Rancid", "Choking Victim", "The Unseen", "The Nerve Agents"]
+    correctAnswer: 3
   },
   {
     question: "This supergroup features members from Leftover Crack, Ensign, & The Ergs.",
-    answer: "Star Fucking Hipsters",
     choices: ["Star Fucking Hipsters", "Me First and the Gimme Gimmes", "The Unseen" "The Transplants"]
+    correctAnswer: 0
   },
   {
     question: "This band's second album is titled 'So This is Freedom.'",
-    answer: "The Unseen",
     choices: ["The Casualties", "The Unseen", "Agnostic Front", "Flogging Molly"]
+    correctAnswer: 1
   }
   {
     question: "The singer from this band has a book titled 'Tranny: Confessions of Punk Rock's Most Infamous Anarchist Sellout'",
-    answer: "Against Me!",
     choices: ["Rancid", "Bad Religion", "Against Me!" "Choking Victum"]
+    correctAnswer: 2
   }
-    ];
-    
+    ];  
+  
+
+    function displayQuestion() {
+        let currentQuestion = triviaData[currentQuestionIndex]; 
+        let questionElement = document.getElementById("question"); 
+        questionElement.innerText = currentQuestion.question; 
+        let choicesElement = document.getElementById("choices"); 
+        choicesElement.innerHTML = ""; 
+        currentQuestion.choices.forEach(choice => { let button = document.createElement("button"); button.innerText = choice; 
+        button.onclick = () => checkAnswer(choice, currentQuestion.answer); choicesElement.appendChild(button); }); }
+
+        
+
+    function checkAnswer(selectedAnswer, correctAnswer) {
+        if (selectedAnswer === correctAnswer) {
+          score++;
+          alert("Yay! You Are Correct!");
+        } else {
+          alert("Sorry, You Got That Answer Incorrect!");
+        }
+        currentQuestionIndex++;
+        if (currentQuestionIndex === questions.length) {
+          endGame();
+        } else {
+          displayQuestion();
+        }
+      }
+ 
+ 
+ 
+      showQuestion(questions[0]);
