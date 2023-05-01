c999390e6fc2be2d48175970445c0132c8a5546d
Jaimilit <>
2023-05-01 12:08:46 +0000

    add trivia questions and answers

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+let triviaData = [
+    {
+        question: "Jello Biaffara was frontman for which punk band?",
+        answer: "The Dead Kennedys",
+        choices: ["The Clash", "The Dead Kennedys", "Rancid","NOFX"]
+      },
+      {
+        question: "Which style of punk was developed by The Misfits?",
+        answer: "Horror Punk",
+        choices: ["Emo Punk", "Hardcore Punk", "Oi Punk", "Horror Punk"]
+      },
+      {
+        question: "In 1995, this band's breakthrough album, Dookie, won the Grammy for Best Alternative Music Album.",
+        answer: "Green Day",
+        choices: ["Green Day", "Rancid", "The Offspring", "NOFX"]
+      }
+      question: "Sometimes, it's the saddest incidents that make for the most amazing of songs. Name the band who turned a suicide tragedy into "Bro Hymn".",
+      answer: "Pennywise",
+      choices: ["Pennywise", "Rancid", "Bad Religion", "Operation Ivy"]
+    },
+    {
+      question: "Milo went to college, and then Everything Sucked. Who is the band, who created these records, and could also be noted for the cartoon drawn about their lead singer, depicting him as an almost Dilbert-like character?",
+      answer: "The Descendents",
+      choices: ["Leftover Crack", "The Descendents", "AFI", "The Unseen"]
+    },
+    {
+      question: "Who is featured in Leftover Crack's 'System Fucked'?",
+      answer: "Jesse Michaels",
+      choices: ["Lars Frederiksen", "Jesse Michaels", "Tim Armstrong", "Jack Terricloth"]
+    }
+    question: "This band was formed by Andy Outbreak and Eric Ozenne and their last album is titled 'The Butterfly Collection'",
+    answer: "The Nerve Agents",
+    choices: ["Rancid", "Choking Victim", "The Unseen", "The Nerve Agents"]
+  },
+  {
+    question: "This supergroup features members from Leftover Crack, Ensign, & The Ergs.",
+    answer: "Star Fucking Hipsters",
+    choices: ["Star Fucking Hipsters", "Me First and the Gimme Gimmes", "The Unseen" "The Transplants"]
+  },
+  {
+    question: "This band's second album is titled 'So This is Freedom.'",
+    answer: "The Unseen",
+    choices: ["The Casualties", "The Unseen", "Agnostic Front", "Flogging Molly"]
+  }
+  {
+    question: "The singer from this band has a book titled 'Tranny: Confessions of Punk Rock's Most Infamous Anarchist Sellout'",
+    answer: "Against Me!",
+    choices: ["Rancid", "Bad Religion", "Against Me!" "Choking Victum"]
+  }
+    ];
+    
