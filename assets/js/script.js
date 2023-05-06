55e99715b788b870052b919296b585bd6da7fec5
Jaimilit <>
2023-05-06 11:32:14 +0000

    add functions and fix questions/grammar in array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,129 @@
 let questions = [
     {
-        question: "Jello Biaffara was frontman for which punk band?",
-        choices: ["The Clash", "The Dead Kennedys", "Rancid","NOFX"],
-        correctAnswer: "The Dead Kennedys"
-      },
-      {
-        question: "Which style of punk was developed by The Misfits?",
-        choices: ["Emo Punk", "Hardcore Punk", "Oi Punk", "Horror Punk"],
-        correctAnswer: "Horror Punk"
-      },
-      {
-        question: "In 1995, this band's breakthrough album, Dookie, won the Grammy for Best Alternative Music Album.",
-        choices: ["Green Day", "Rancid", "The Offspring", "NOFX"],
-        correctAnswer: "Green Day"
-      }
-      question: "Sometimes, it's the saddest incidents that make for the most amazing of songs. Name the band who turned a suicide tragedy into "Bro Hymn".",
-      choices: ["Pennywise", "Rancid", "Bad Religion", "Operation Ivy"],
-      correctAnswer: "Pennywise"
+        question: 'Jello Biaffara was frontman for which punk band?',
+        answers: [
+            { text: 'The Clash', correct: false },
+            { text: 'The Dead Kennedys', correct: true },
+            { text: 'Rancid', correct: false },
+            { text: 'NOFX', correct: false },
+        ]
+       
     },
     {
-      question: "Milo went to college, and then Everything Sucked. Who is the band, who created these records, and could also be noted for the cartoon drawn about their lead singer, depicting him as an almost Dilbert-like character?",
-      choices: ["Leftover Crack", "The Descendents", "AFI", "The Unseen"],
-      correctAnswer: "The Descendents"
+        question: 'Which style of punk was developed by The Misfits?',
+        answers: [
+            { text: 'Emo Punk', correct: false },
+            { text: 'Hardcore Punk', correct: false },
+            { text: 'Oi Punk', correct: false },
+            { text: 'Horror Punk', correct: true },
+        ]
     },
     {
-      question: "Who is featured in Leftover Crack's 'System Fucked'?",
-      choices: ["Lars Frederiksen", "Jesse Michaels", "Tim Armstrong", "Jack Terricloth"],
-      correctAnswer: "Jesse Michaels"
-    }
-    question: "This band was formed by Andy Outbreak and Eric Ozenne and their last album is titled 'The Butterfly Collection'",
-    choices: ["Rancid", "Choking Victim", "The Unseen", "The Nerve Agents"],
-    correctAnswer: "The Nerve Agents"
+        question: 'In 1995, this band\'s breakthrough album, Dookie, won the Grammy for Best Alternative Music Album.',
+        answers: [
+            { text: 'Green Day', correct: true },
+            { text: 'Rancid', correct: false },
+            { text: 'NOFX', correct: false },
+            { text: 'The Offspring', correct: false },
+        ]
+    
+    },
+    {
+        question: 'Sometimes, it\'s the saddest incidents that make for the most amazing of songs. Name the band who turned a suicide tragedy into "Bro Hymn"',
+        answers: [
+            { text: 'Pennywise', correct: true },
+            { text: 'Bad Religion', correct: false },
+            { text: 'Operation Ivy', correct: false },
+            { text: 'Rancid', correct: false },
+        ]
+    },
+    {
+        question: 'Milo went to college, and then Everything Sucked. Who is the band, who created these records, and could also be noted for the cartoon drawn about their lead singer, depicting him as an almost Dilbert-like character?',
+        answers: [
+            { text: 'Leftover Crack', correct: false },
+            { text: 'The Descendents', correct: true },
+            { text: 'AFI', correct: false },
+            { text: 'The Unseen', correct: false },
+        ]
+    },
+    {
+      question: 'Who is featured in Leftover Crack's song titled ystem Fucked?',
+      answers: [
+        { text: 'Lars Frederiksen', correct: false },
+        { text: 'Jesse Michaels', correct: true },
+        { text: 'Tim Armstrong', correct: false },
+        { text: 'Jack Terricloth', correct: false },
+    ]
+    },
+    {
+    question: 'This band was formed by Andy Outbreak and Eric Ozenne and their last album is titled "The Butterfly Collection"',
+    answers: [
+        { text: 'Rancid', correct: false },
+        { text: 'Choking Victim', correct: false },
+        { text: 'The Unseen', correct: false },
+        { text: 'The Nerve Agents', correct: true },
+    ]
+},
+  {
+    question: 'This supergroup features members from Leftover Crack, Ensign, & The Ergs.',
+    answers: [
+        { text: 'Star Fucking Hipsters', correct: true },
+        { text: 'Me First and the Gimme Gimmes', correct: false },
+        { text: 'Transplants', correct: false },
+        { text: 'The Unseen', correct: false },
+    ]
   },
   {
-    question: "This supergroup features members from Leftover Crack, Ensign, & The Ergs.",
-    choices: ["Star Fucking Hipsters", "Me First and the Gimme Gimmes", "The Unseen", "The Transplants"],
-    correctAnswer: "Star Fucking Hipsters"
+    question: 'This band's second album is titled "So This is Freedom."',
+    answers: [
+        { text: 'The Casualties', correct: false },
+        { text: 'The Unseen', correct: true },
+        { text: 'Agnostic Front', correct: false },
+        { text: 'Flogging Molly', correct: false },
+    ]
   },
   {
-    question: "This band's second album is titled 'So This is Freedom.'",
-    choices: ["The Casualties", "The Unseen", "Agnostic Front", "Flogging Molly"],
-    correctAnswer: "The Unseen"
-  }
-  {
-    question: "The singer from this band has a book titled 'Tranny: Confessions of Punk Rock's Most Infamous Anarchist Sellout'",
-    choices: ["Rancid", "Bad Religion", "Against Me!", "Choking Victum"],
-    correctAnswer: "Against Me!"
-  }
+    question: 'The singer from this band has a book titled "Tranny: Confessions of Punk Rock's Most Infamous Anarchist Sellout"',
+    answers: [
+        { text: 'Rancid', correct: false },
+        { text: 'Bad Religion', correct: false },
+        { text: 'Against Me!', correct: true },
+        { text: 'Choking Victum', correct: false },
+    ]
+  },
     ];  
   
 
-    let currentQuestion = 0;
-		let score = 0;
+let startButton = document.getElementById('start-btn')
+let questionContainerElement = document.getElementById('question-container')
+let shuffledQuestions, currentQuestionIndex
+let questionElement = document.getElementById('question')
+let answerButtonsElement = document.getElementById('answer-buttons')
 
-        function displayQuestion() {
-            document.getElementById("question").textContent = questions[currentQuestion].question;
-            document.getElementById("choices").innerHTML = "";
-            for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
-                let choice = document.createElement("button");
-                choice.textContent = questions[currentQuestion].choices[i];
-                choice.addEventListener("click", function() {
-                    checkAnswer(choice.textContent);
-                });
-                document.getElementById("choices").appendChild(choice);
-            }
-        }
 
-        displayQuestion();
+
+startButton.addEventListener('click', startGame)
+
+function startGame () {
+console.log('Started')
+startButton.classList.add('hide')
+shuffledQuestions = questions.sort((>= Math.random() - .5))
+currentQuestionIndex = 0
+questionContainerElement.classList.remove('hide')
+SetNextQuestion()
+}
+
+function nextQuestion() {
+showQuestion (shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion (question) {
+question.element.innerText = question.question
+}
+
+function selectAnswer() {
+
+}
 
    
     
