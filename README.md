159499605dab9c5a1dc44ccb80397aae060e050d
Jaimilit <>
2023-05-18 12:42:23 +0000

    shift option f to align code and add more to readme

--- README.md
+++ README.md
@@ -25,6 +25,8 @@ Start Page
 The title always appears on the screen.
 The start button I made big so the user cannot miss it. 
 
+! [Start Page](../Project-Two/assets/images/Start-page.jpg)
+
 Questions
 
 The user is presented with a 10 questions quiz game about punk rock music. 
@@ -36,18 +38,32 @@ The user immedately knows how he/she is doing on the quiz. Once chosen an answer
 All the buttons are the same size for the choices. The start button and next are big for easy access for the user.
 The user also has to choose a response before the next button appears. This is so he/she does not skip a question and is forced to provide an answer first. 
 
-
 The scoreboard also keeps tally of correct and incorrect responses so that the user knows how he/she is doing at all times during the game. This way, at the end there will be no surprises in the final tally.
+
+! [Questions Page](../Project-Two/assets/images/Questions.jpg)
 
 End Page
 At the end of the quiz the score board is removed and a final score is provided. It also announces to the user that the game is over. 
 The user is provided with a restart button if he/she would like to try again. I kept the button small as I wanted the focus to be on the final score for the user.
+! [End Page](../Project-Two/assets/images/Game%20Over.jpg)
 
 
 Footer
 The footer was insprired by the love running project and the icons were taken from font awesome.
 
 Testing
+| Goal | Execution |
+| --------------- | --------------- |
+| The main goal is to let the user test his/her punk rock knowledge.  | The quiz tests a person's knowledge and restarts if they want to try and improve.|
+| The user should have good insight of the content when viewing the startpage. | The title and background gives the user clear insight on what the quiz is about  |
+| The user should have an easy accesible button to start the quiz.  | The start button is easily visible  |
+| The user should receive direct feedback on whether the answer is correct or not.  | A clear red and green color is used for the user to easily see which option was correct.  |
+| The user should have the possiblity to restart the quiz and try to improve.  | When the quiz finishes the user receives a restart button if they want to test their knowledge again  |
+
+Responsiveness
+
+The website is adapted to be seen on different screen sizes. The responsiveness of the website was first tested by chrome developer tools. Different breakpoints where used to view the website. Next to this the website was viewed on different devices: laptop, tablet and smartphones.
+
 I have tested that my projects works on various browsers - Safari, Chrome, and FireFox. I also have confirmed that my project is responsive by using the devtools toolbar. I confirm that all sections, heading, body, & footer, are readable and easy to understand. 
 
 Validator Testing
@@ -57,14 +73,18 @@ Accessibility I confirm easy user accessibility by running it through lighthouse
 lighthouse CSS html
 
 Unfixed Bugs
-In the future I would like to increase my accessibility score, as well as my performance score. I think the more I work at coding and become familiar with best practices, that these scores will become better and better.
+In the future I would like to increase my accessibility score, as well as my performance score. I think the more I work at coding and become familiar with best practices, that these scores will become better and better. In the future I would like to add a timer to countdown for each question.
 
 Deployment
 For this project I used GitHub
 
-In the GitHub repository, navigate to the Settings tab
-From the source section drop-down menu, select the Master Branch
-Once the master branch has been selected, the page will be automatically refreshed with a detailed ribbon display to indicate the successful deployment.
+* Log into GitHub.
+* Locate the GitHub Repository that shall be deployed live.
+* In the GitHub repository, navigate to the Settings tab
+* From the source section drop-down menu, select the Master Branch
+* At the ”Source” section choose ”main” as Branch and root as folder and click ”Save”
+* The website will deploy and the pages refreshes to provide the live link to the project.
+
 The live link can be found here - https://github.com/Jaimilit/Project-Two/settings/pages
 
 Credits
@@ -74,7 +94,4 @@ The icons in the footer were taken from Font Awesome
 Footer was taken from Love Running Project
 Trivia questions were written by me and inspired by these two websites: https://www.funtrivia.com/trivia-quiz/Music/So-You-Think-Youre-Punk-170784.html and https://thereviewsarein.com/2020/05/03/pretty-hard-punk-rock-quiz/
 Wallpaper background taken from here: https://wallpaperaccess.com/punk-rock
-
-
-
-
+Much of the code was inspried by this video: https://www.youtube.com/watch?v=riDzcEQbX6k
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,251 +1,246 @@
 // game questions
 const questions = [
-    {
-        question: 'Jello Biafra was frontman for which punk band?',
-        answers: [
-            { text: 'The Clash', correct: false },
-            { text: 'The Dead Kennedys', correct: true },
-            { text: 'Rancid', correct: false },
-            { text: 'NOFX', correct: false },
-        ]
-    },
-    {
-        question: 'Which style of punk was developed by The Misfits?',
-        answers: [
-            { text: 'Emo Punk', correct: false },
-            { text: 'Hardcore Punk', correct: false },
-            { text: 'Oi Punk', correct: false },
-            { text: 'Horror Punk', correct: true },
-        ]
-    },
-    {
-        question: 'In 1995, this band\'s breakthrough album, "Dookie", won the Grammy for Best Alternative Music Album.',
-        answers: [
-            { text: 'Green Day', correct: true },
-            { text: 'Rancid', correct: false },
-            { text: 'NOFX', correct: false },
-            { text: 'The Offspring', correct: false },
-        ]
-   
-    },
-    {
-        question: 'Sometimes, it\'s the saddest incidents that make for the most amazing of songs. Name the band who turned a suicide tragedy into "Bro Hymn."',
-        answers: [
-            { text: 'Pennywise', correct: true },
-            { text: 'Bad Religion', correct: false },
-            { text: 'Operation Ivy', correct: false },
-            { text: 'Rancid', correct: false },
-        ]
-    },
-    {
-        question: 'Tim Armstrong & Lars Frederiksen are singers of this famous punk band.',
-        answers: [
-            { text: 'Leftover Crack', correct: false },
-            { text: 'Rancid', correct: true },
-            { text: 'AFI', correct: false },
-            { text: 'The Unseen', correct: false },
-        ]
-    },
-    {
-      question: 'Who was the lead singer of Operation Ivy?',
-      answers: [
-        { text: 'Lars Frederiksen', correct: false },
-        { text: 'Jesse Michaels', correct: true },
-        { text: 'Tim Armstrong', correct: false },
-        { text: 'Jack Terricloth', correct: false },
-    ]
-    },
-    {
-    question: 'This band was formed by Andy Outbreak and Eric Ozenne and their last album is titled "The Butterfly Collection."',
-    answers: [
-        { text: 'Rancid', correct: false },
-        { text: 'Choking Victim', correct: false },
-        { text: 'The Unseen', correct: false },
-        { text: 'The Nerve Agents', correct: true },
-    ]
- },
   {
-    question: 'This supergroup features members from Leftover Crack, Ensign, & The Ergs.',
+    question: "Jello Biafra was frontman for which punk band?",
     answers: [
-        { text: 'Star Fucking Hipsters', correct: true },
-        { text: 'Me First and the Gimme Gimmes', correct: false },
-        { text: 'Transplants', correct: false },
-        { text: 'The Unseen', correct: false },
-    ]
+      { text: "The Clash", correct: false },
+      { text: "The Dead Kennedys", correct: true },
+      { text: "Rancid", correct: false },
+      { text: "NOFX", correct: false },
+    ],
+  },
+  {
+    question: "Which style of punk was developed by The Misfits?",
+    answers: [
+      { text: "Emo Punk", correct: false },
+      { text: "Hardcore Punk", correct: false },
+      { text: "Oi Punk", correct: false },
+      { text: "Horror Punk", correct: true },
+    ],
+  },
+  {
+    question:
+      'In 1995, this band\'s breakthrough album, "Dookie", won the Grammy for Best Alternative Music Album.',
+    answers: [
+      { text: "Green Day", correct: true },
+      { text: "Rancid", correct: false },
+      { text: "NOFX", correct: false },
+      { text: "The Offspring", correct: false },
+    ],
+  },
+  {
+    question:
+      'Sometimes, it\'s the saddest incidents that make for the most amazing of songs. Name the band who turned a suicide tragedy into "Bro Hymn."',
+    answers: [
+      { text: "Pennywise", correct: true },
+      { text: "Bad Religion", correct: false },
+      { text: "Operation Ivy", correct: false },
+      { text: "Rancid", correct: false },
+    ],
+  },
+  {
+    question:
+      "Tim Armstrong & Lars Frederiksen are singers of this famous punk band.",
+    answers: [
+      { text: "Leftover Crack", correct: false },
+      { text: "Rancid", correct: true },
+      { text: "AFI", correct: false },
+      { text: "The Unseen", correct: false },
+    ],
+  },
+  {
+    question: "Who was the lead singer of Operation Ivy?",
+    answers: [
+      { text: "Lars Frederiksen", correct: false },
+      { text: "Jesse Michaels", correct: true },
+      { text: "Tim Armstrong", correct: false },
+      { text: "Jack Terricloth", correct: false },
+    ],
+  },
+  {
+    question:
+      'This band was formed by Andy Outbreak and Eric Ozenne and their last album is titled "The Butterfly Collection."',
+    answers: [
+      { text: "Rancid", correct: false },
+      { text: "Choking Victim", correct: false },
+      { text: "The Unseen", correct: false },
+      { text: "The Nerve Agents", correct: true },
+    ],
+  },
+  {
+    question:
+      "This supergroup features members from Leftover Crack, Ensign, & The Ergs.",
+    answers: [
+      { text: "Star Fucking Hipsters", correct: true },
+      { text: "Me First and the Gimme Gimmes", correct: false },
+      { text: "Transplants", correct: false },
+      { text: "The Unseen", correct: false },
+    ],
   },
   {
     question: 'This band has a second album titled "So This is Freedom"?',
     answers: [
-        { text: 'The Casualties', correct: false },
-        { text: 'The Unseen', correct: true },
-        { text: 'Agnostic Front', correct: false },
-        { text: 'Flogging Molly', correct: false },
-    ]
+      { text: "The Casualties", correct: false },
+      { text: "The Unseen", correct: true },
+      { text: "Agnostic Front", correct: false },
+      { text: "Flogging Molly", correct: false },
+    ],
   },
   {
-    question: 'The singer from this band wrote a memoir titled "Tranny: Confessions of Punk Rock Most Infamous Anarchist Sellout."',
+    question:
+      'The singer from this band wrote a memoir titled "Tranny: Confessions of Punk Rock Most Infamous Anarchist Sellout."',
     answers: [
-        { text: 'Rancid', correct: false },
-        { text: 'Bad Religion', correct: false },
-        { text: 'Against Me!', correct: true },
-        { text: 'Choking Victum', correct: false },
-    ]
+      { text: "Rancid", correct: false },
+      { text: "Bad Religion", correct: false },
+      { text: "Against Me!", correct: true },
+      { text: "Choking Victum", correct: false },
+    ],
   },
-    ]; 
+];
 
+// define variables in order to drive quiz
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const scoreAreaElement = document.querySelector(".score-area");
+const answerButtonsElement = document.getElementById("answer-buttons");
+const restartButton = document.getElementById("restart-btn");
+let shuffledQuestions;
+let currentQuestionIndex;
+let score = 0;
+let answerSelected = false;
 
-     // define variables in order to drive quiz
-  const startButton = document.getElementById('start-btn');
-  const nextButton = document.getElementById('next-btn');
-  const questionContainerElement = document.getElementById('question-container');
-  const questionElement = document.getElementById('question');
-  const scoreAreaElement = document.querySelector('.score-area');
-  const answerButtonsElement = document.getElementById('answer-buttons');
-  const restartButton = document.getElementById('restart-btn');
-   let shuffledQuestions;
-  let currentQuestionIndex;
-  let score = 0;
-  let answerSelected = false; 
- 
- 
- // event listeners
- startButton.addEventListener('click', startGame);
- nextButton.addEventListener('click', () => {
-   currentQuestionIndex++;
-   setNextQuestion();
- });
- restartButton.addEventListener('click', restartGame);
+// event listeners
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
+restartButton.addEventListener("click", restartGame);
 
-
-   // function to start game
-   function startGame () {
-    startButton.classList.add('hide');
-    shuffledQuestions = questions.sort(() => Math.random() - .5);
-    currentQuestionIndex = 0;
-    score = 0;
-    questionContainerElement.classList.remove('hide');
-    scoreAreaElement.classList.remove('hide');
-    setNextQuestion();
-    document.querySelector('.score-area').style.display = "block";
+// function to start game
+function startGame() {
+  startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  score = 0;
+  questionContainerElement.classList.remove("hide");
+  scoreAreaElement.classList.remove("hide");
+  setNextQuestion();
+  document.querySelector(".score-area").style.display = "block";
 }
 
-   // function to go to the next question and shuffle the order
- 
-   function setNextQuestion() {
-    resetState();
-    if (currentQuestionIndex >= 10) {
-      endGame();
-    } else {
-      showQuestion(shuffledQuestions[currentQuestionIndex]);
-    }
-  }
- 
-   
- 
-   //function to show next question and add button
- 
-  function showQuestion(question) {
-    questionElement.innerText = question.question;
-    question.answers.forEach(answer => {
-      const button = document.createElement('button');
-      button.innerText = answer.text;
-      button.classList.add('btn');
-      if (answer.correct) {
-        button.dataset.correct = answer.correct;
-      }
-      button.addEventListener('click', selectAnswer);
-      answerButtonsElement.appendChild(button);
-    });
-  }
- 
-  // removes buttons when going to new question
-  function resetState() {
-    clearStatusClass(document.body);
-    nextButton.classList.add('hide');
-    while (answerButtonsElement.firstChild) {
-      answerButtonsElement.removeChild(answerButtonsElement.firstChild);
-    }
-  }
- 
- 
- // checks if selected answer is correct or incorrect and increments it accordingly
- function selectAnswer(e) {
-    const selectedButton = e.target;
-    const correct = selectedButton.dataset.correct;
-    setStatusClass(document.body, correct);
-    Array.from(answerButtonsElement.children).forEach(button => {
-      setStatusClass(button, button.dataset.correct);
-      button.disabled = true; // disable all buttons in answerButtonsElement
-    });
-  
-    if (correct) {
-      incrementScore();
-    } else {
-      incrementWrongAnswer();
-    }
-    // show the next button if there are more questions
-    if (shuffledQuestions.length > currentQuestionIndex + 1) {
-      nextButton.classList.remove('hide');
-    } else {
-      endGame();
-    }
-  }
- 
- 
-  function setStatusClass(element, correct) {
-    clearStatusClass(element);
-    if (correct) {
-      element.classList.add('correct');
-    } else {
-      element.classList.add('wrong');
-    }
-  }
-   function clearStatusClass(element) {
-    element.classList.remove('correct');
-    element.classList.remove('wrong');
-  }
-  function incrementScore() {
-    let oldScore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = ++oldScore;
-    score = oldScore;
-  }
- 
-   function incrementWrongAnswer() {
-       let oldScore = parseInt(document.getElementById("incorrect").innerText);
-   document.getElementById("incorrect").innerText = ++oldScore;
-   }
- 
-        // end game
-        function endGame() {
-            const message = `You scored ${score} out of 10`;
-            const scoreElement = document.createElement('p');
-            scoreElement.innerText = message;
-            questionContainerElement.appendChild(scoreElement);
-            const gameOverElement = document.createElement('h2');
-            gameOverElement.innerText = 'Game over!';
-            questionContainerElement.appendChild(gameOverElement);
-            scoreAreaElement.style.display = "none";
-            restartButton.classList.remove('hide'); // Show restart button
-          }
+// function to go to the next question and shuffle the order
 
+function setNextQuestion() {
+  resetState();
+  if (currentQuestionIndex >= 10) {
+    endGame();
+  } else {
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+  }
+}
 
-          function restartGame() {
-            currentQuestionIndex = 0;
-            score = 0;
-            answerSelected = false;
-            shuffledQuestions = questions.sort(() => Math.random() - .5);
-            startGame(); // call the startGame() function to restart the game
-            restartButton.classList.add('hide');
-            const scoreElement = document.querySelector('#question-container p');
-            if (scoreElement) {
-              scoreElement.remove();
-            }
-            const gameOverElement = document.querySelector('#question-container h2');
-            if (gameOverElement) {
-              gameOverElement.remove();
-            }
-            document.getElementById("score").innerText = '0';
-            document.getElementById("incorrect").innerText = '0';
-          }
- 
- 
+//function to show next question and add button
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+// removes buttons when going to new question
+function resetState() {
+  clearStatusClass(document.body);
+  nextButton.classList.add("hide");
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
+// checks if selected answer is correct or incorrect and increments it accordingly
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+    button.disabled = true; // disable all buttons in answerButtonsElement
+  });
+
+  if (correct) {
+    incrementScore();
+  } else {
+    incrementWrongAnswer();
+  }
+  // show the next button if there are more questions
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    endGame();
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
+function incrementScore() {
+  let oldScore = parseInt(document.getElementById("score").innerText);
+  document.getElementById("score").innerText = ++oldScore;
+  score = oldScore;
+}
+
+function incrementWrongAnswer() {
+  let oldScore = parseInt(document.getElementById("incorrect").innerText);
+  document.getElementById("incorrect").innerText = ++oldScore;
+}
+
+// end game
+function endGame() {
+  const message = `You scored ${score} out of 10`;
+  const scoreElement = document.createElement("p");
+  scoreElement.innerText = message;
+  questionContainerElement.appendChild(scoreElement);
+  const gameOverElement = document.createElement("h2");
+  gameOverElement.innerText = "Game over!";
+  questionContainerElement.appendChild(gameOverElement);
+  scoreAreaElement.style.display = "none";
+  restartButton.classList.remove("hide"); // Show restart button
+}
+
+function restartGame() {
+  currentQuestionIndex = 0;
+  score = 0;
+  answerSelected = false;
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  startGame(); // call the startGame() function to restart the game
+  restartButton.classList.add("hide");
+  const scoreElement = document.querySelector("#question-container p");
+  if (scoreElement) {
+    scoreElement.remove();
+  }
+  const gameOverElement = document.querySelector("#question-container h2");
+  if (gameOverElement) {
+    gameOverElement.remove();
+  }
+  document.getElementById("score").innerText = "0";
+  document.getElementById("incorrect").innerText = "0";
+}
